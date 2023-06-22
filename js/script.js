adfebe1bf820c57e32a5bb28b56bba7f8ff7058b
ASTIK <astik.ytt@gmail.com>
2023-06-22 15:30:13 +0200

    Update generate functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let hairFolder = 1;
+
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -5,12 +7,20 @@ function randomNumber(min, max) {
 function changeImg(name, maxN) {
 	let newImgNumber = randomNumber(1, maxN);
 	let imgElem = document.querySelector(`.character__${name}`);
+
+	if (name == "face") {
+		hairFolder = newImgNumber;
+	} else if (name == "hair") {
+		imgElem.src = `img/${name}/${hairFolder}/${newImgNumber}.png`;
+		return;
+	}
+
 	imgElem.src = `img/${name}/${newImgNumber}.png`;
 }
 
 function generateNewCharacter() {
-	changeImg("hair", 81);
 	changeImg("face", 9);
+	changeImg("hair", 9);
 	changeImg("gloves", 9);
 	changeImg("shorts", 9);
 }
